f31916f529ff694f6db98c2e710862cae093358e
sankhamalapal <sankhamala13@gmail.com>
2026-04-16 15:52:23 +0200

    Created Spring Boot Maven Project.

---
+++ src/main/java/dev/sankha/restaurant_finder/Application.java
@@ -0,0 +1,13 @@
+package dev.sankha.restaurant_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantFinderApplication.class, args);
+    }
+
+}
